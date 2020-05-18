7d5855fab52a29472ad0a999e1d9b4495b0cceb1
ThuanDuong <Thuan_Duong@epam.com>
2020-05-18 18:34:14 +0700

    update test case login

--- src/test/java/org/mytests/tests/data/UserDataProvider.java
+++ src/test/java/org/mytests/tests/data/UserDataProvider.java
@@ -4,29 +4,39 @@ import org.testng.annotations.DataProvider;
 
 public class UserDataProvider {
     public static LoginData INCORRECT_USERNAME_PASSWORD = new LoginData().set(c -> {
-        c.loginInfo = new User().set(d->{d.username = "thuanduongtest21@gmail.com"; d.password="Thuan12";});
+        c.loginInfo = new User().set(d -> {
+            d.username = "thuanduongtest21@gmail.com";
+            d.password = "Thuan12";
+        });
         c.errorMessage = "Error: A user could not be found with this email address.";
     });
 
     public static LoginData CORRECT_USERNAME_EMPTY_PASSWORD = new LoginData().set(c -> {
-        c.loginInfo = new User().set(d->{d.username = "thuanduongtest2@gmail.com"; d.password="";});
+        c.loginInfo = new User().set(d -> {
+            d.username = "thuanduongtest2@gmail.com";
+            d.password = "";
+        });
         c.errorMessage = "Error: Password is required.";
     });
 
-    public static User NO_CREDENTIALS = new User().set(c -> {
-        c.username = ""; c.password = "";}
-    );
-    public static User WRONG_CREDENTIALS = new User().set(c -> {
-        c.username = "Alex"; c.password = "Password";}
-    );
+    public static LoginData EMPTY_USERNAME_VALID_PASSWORD = new LoginData().set(c -> {
+        c.loginInfo = new User().set(d -> {
+            d.username = "";
+            d.password = "Thuan120891";
+        });
+        c.errorMessage = "Error: Username is required.";
+    });
 
-//    @DataProvider(name = "failedUsers")
-//    public static Object[][] failedUsers() {
-//        return new User[][]{{NO_CREDENTIALS}, {WRONG_CREDENTIALS}};
-//    }
+    public static LoginData EMPTY_USERNAME_EMPTY_PASSWORD = new LoginData().set(c -> {
+        c.loginInfo = new User().set(d -> {
+            d.username = "";
+            d.password = "";
+        });
+        c.errorMessage = "Error: Username is required.";
+    });
 
     @DataProvider(name = "failedUsers")
     public static Object[][] failedUsers() {
-        return new LoginData[][]{{INCORRECT_USERNAME_PASSWORD}, {CORRECT_USERNAME_EMPTY_PASSWORD}};
+        return new LoginData[][]{{INCORRECT_USERNAME_PASSWORD}, {CORRECT_USERNAME_EMPTY_PASSWORD}, {EMPTY_USERNAME_VALID_PASSWORD}, {EMPTY_USERNAME_EMPTY_PASSWORD}};
     }
 }
--- src/test/java/org/mytests/tests/example/LoginTests.java
+++ src/test/java/org/mytests/tests/example/LoginTests.java
@@ -3,39 +3,26 @@ package org.mytests.tests.example;
 import org.mytests.tests.SimpleTestsInit;
 import org.mytests.tests.data.LoginData;
 import org.mytests.tests.data.UserDataProvider;
-import org.mytests.uiobjects.example.entities.User;
 import org.testng.annotations.Test;
 
 import static org.mytests.uiobjects.example.TestData.*;
 import static org.mytests.uiobjects.example.site.JdiTestSite.myAccountPage;
 import static org.mytests.uiobjects.example.site.pages.MyAccountPage.*;
-import static org.testng.Assert.assertEquals;
 
 public class LoginTests implements SimpleTestsInit {
-
-
-
-//    public static LoginData CORRECT_USERNAME_EMPTY_PASSWORD = new LoginData().set(c -> {
-//        c.loginInfo.username = "thuanduongtest2@gmail.com";
-//        c.loginInfo.password = "";
-//        c.errorMessage = "Error: Password is required.";
-//    });
-
-
 
 
     @Test(dataProvider = "failedUsers", dataProviderClass = UserDataProvider.class)
     public void loginIncorrectUser(LoginData loginData) {
         myAccountPage.open();
         loginForm.loginAs(loginData.loginInfo);
-//        errorLoginMessage.has().attr("outerText", loginData.errorMessage);
     }
 
-//    @Test
-//    public void loginTest() {
-//        myAccountPage.open();
-//        loginForm.loginAs(DEFAULT_USER);
-//        assert userNameLoggedIn.isDisplayed();
-//    }
+    @Test
+    public void loginTest() {
+        myAccountPage.open();
+        loginForm.loginAs(DEFAULT_USER);
+        assert userNameLoggedIn.isDisplayed();
+    }
 
 }
